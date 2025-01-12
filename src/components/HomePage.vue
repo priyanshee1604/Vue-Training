ac419c9612813c675f0e39935bdac3af4b50cf16
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-01-12 13:56:39 +0530

    ecomm home page with dead links

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<!-- Create a  new branch from the last task branch 
+ and Implement e-commerce web application 
+ with a home page and top navigation with dead links.
+ -->
+<template>
+   <div>HOME PAGE</div>
+   <router-link to="#">Category</router-link><br>
+   <router-link to="#">Products</router-link>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "@/components/HomePage.vue";
+
+const routes= [
+    {
+        path: '/',
+        component: HomePage
+    },
+
+];
+
+const router = createRouter({ 
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
