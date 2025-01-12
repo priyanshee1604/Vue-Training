f2f7f1378e45f6c70083954898e4f28cfe53438c
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-01-12 22:29:53 +0530

    added feature to sort and filter products

--- src/components/CategoryPage.vue
+++ src/components/CategoryPage.vue
@@ -2,10 +2,25 @@
  and add a category page which lists all products belonging to the category.  
  Create a new PR with the last task branch as the target branch.
  -->
+
+ <!-- Add features to sort and filter out products based upon price, color or any other field on the category page -->
 <template>
-    <div>Products according to the category.</div><br>
+    <p>Sort Products:</p>
+    <select v-model="sortby" @change="sortingProduct">
+        <option value="price">Price</option>
+        <option value="rating">Rating</option>
+    </select>
+    <br><br>
+    <p> Filter products based on Brands</p>
+    <input type="radio" name="brand" value="Apple" v-model="selectedBrand" @change="filterByBrand">Apple
+    <input type="radio" name="brand" value="Samsung" v-model="selectedBrand" @change="filterByBrand">Samsung
+    <input type="radio" name="brand" value="Vivo" v-model="selectedBrand" @change="filterByBrand">Vivo
+    <input type="radio" name="brand" value="Realme" v-model="selectedBrand" @change="filterByBrand">Realme
+    <input type="radio" name="brand" value="Oppo" v-model="selectedBrand" @change="filterByBrand">Oppo
+    <br><br>
+    <div>Products according to the category.</div>
     <ol>
-        <li v-for="product in products[0]" v-bind:key="product.id">{{ product }}</li>
+        <li v-for="product in products" v-bind:key="product.id">{{ product }}</li>
     </ol>
 </template>
 
@@ -15,7 +30,9 @@ import axios from 'axios';
         data(){
             return{
                 res: [],
-                products: []
+                products: [],
+                sortby: 'price',
+                selectedBrand:''
             };
         },
 
@@ -27,6 +44,26 @@ import axios from 'axios';
         })
            .catch(err=>console.log(err));
         },
+
+        methods:{
+            sortingProduct(){
+                if(this.sortby=='price')
+            {
+                this.products.sort((a,b)=>a.price-b.price);
+            }else{
+                this.products.sort((a,b)=>a.rating-b.rating);
+
+            }
+        },
+        filterByBrand() {
+            console.log(this.selectedBrand);
+            this.products = this.products.filter(product => {
+             return product.brand === this.selectedBrand;
+        });
+}
+        
+
+    }
     }
 
 </script>
