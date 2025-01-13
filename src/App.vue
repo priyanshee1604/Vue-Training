c4c2ec04f691b4944832a6f348445db23ced0fdd
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-01-13 12:15:16 +0530

    Storing token in localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RegistrationPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import RegistrationPage from './components/RegistrationPage.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegistrationPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
